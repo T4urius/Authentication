76a2c8580cb1539b1eb1a759032497cb13975aa0
Matheus Andrade Uliana <matheus.andrade@sawluz.com.br>
2019-10-22 10:54:39 -0300

    Criando Dtos

---
+++ BackEnd/ProjectAuthentication/ProjectAuthentication/Dtos/RegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAuthentication.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Email deve ter no máximo 3 caracteres")]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Email deve ter no máximo 3 caracteres")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Sua senha deve ter entre 8 a 20 caracteres")]
+        public string Password { get; set; }
+    }
+}
