a95be7b3a4e032e6695ff79247223c2e6b11586c
Matheus Andrade Uliana <matheus.andrade@sawluz.com.br>
2019-10-23 13:43:27 -0300

    book e bookservice

--- FrontEnd/src/app/services/book.service.ts
+++ FrontEnd/src/app/services/book.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Book } from '../book/book';
+import { TouchSequence } from 'selenium-webdriver';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:44348/api/book';
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl + 'book')
+      .pipe(
+        tap(_ => this.log('fetched books')),
+        catchError(this.handleError('getBooks', []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log(message);
+  }
 }
